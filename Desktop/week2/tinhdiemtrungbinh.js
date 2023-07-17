8325e4005a5ed6cb2c57d1650e8566516bcbbaa5
DungBeo2610 <ducdungdo40@gmail.com>
2023-07-17 09:51:26 +0700

    d1

---
+++ Desktop/week2/tinhdiemtrungbinh.js
@@ -0,0 +1,9 @@
+inputPhysic = prompt("Enter the Physic");
+inputSience = prompt("Enter the Sience");
+inputLiterature = prompt("Enter the Literature");
+let Physic = parseInt(inputPhysic);
+let Sience = parseInt(inputSience);
+let Literature = parseInt(inputLiterature);
+let Average = (Physic + Sience + Literature)/3;
+document.write("The Average is "+Average);
+
